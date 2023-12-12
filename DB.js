cc2473fd175f0878714487b008ab228c3a3edbad
Tanay1208 <99310413+Tanay1208@users.noreply.github.com>
2023-12-12 03:44:02 +0000

    Add files via upload

---
+++ DB.js
@@ -0,0 +1,43 @@
+import { default as mongodb } from "mongodb";
+let MongoClient = mongodb.MongoClient;
+let ObjectId = mongodb.ObjectId;
+
+let connection = await MongoClient.connect("mongodb://localhost:27017/", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const DB = connection.db("Gymnast");
+
+const userCollection = "User";
+
+const vehicleCollection="Player"
+
+const InventoryCollection = "InventoryCollection"
+
+const MAGSCollection="MAGScoresheet"
+
+const WAGSCollection="WAGScoresheet"
+
+const MAGConsolidated="MAGConsolidated"
+
+const WAGConsolidated="WAGConsolidated"
+
+const ItemSellCollection = "ItemSell"
+
+const RevenueCount = "RevenueCount"
+
+export default DB;
+export {
+	ObjectId,
+	userCollection,
+	vehicleCollection,
+	MAGSCollection,
+	WAGSCollection,
+	MAGConsolidated,
+	WAGConsolidated,
+	InventoryCollection,
+	ItemSellCollection,
+	RevenueCount
+
+};
